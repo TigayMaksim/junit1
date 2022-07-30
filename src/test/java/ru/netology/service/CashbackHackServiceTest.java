d118b02ac5a7f25cc6d6a69a57090d45bb992cc7
Тигай Максим <timati.87@list.ru>
2022-07-30 15:26:21 +0300

    Platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,9 @@
 package ru.netology.service;
 
 
-
 import org.junit.Test;
+
 import static org.junit.Assert.*;
-
-
 
 
 public class CashbackHackServiceTest {
@@ -13,24 +11,24 @@ public class CashbackHackServiceTest {
 
     @Test
 
-   public void shouldFindAmount1() {
+    public void shouldFindAmount1() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = 100;
-        int expected =100;
-        assertEquals(expected,actual);
+        int expected = 100;
+        assertEquals(expected, actual);
     }
 
 
     @Test
 
-   public void shouldFindAmount2() {
+    public void shouldFindAmount2() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = 0;
-        int expected =100;
+        int expected = 100;
 
         assertEquals(expected, actual);
     }
@@ -43,8 +41,43 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = 0;
-        int expected =0;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
+
+
+    @org.junit.jupiter.api.Test
+    public void shouldFindAmount4() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = 100;
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldFindAmount5() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = 0;
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldFindAmount6() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = 0;
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
 }
